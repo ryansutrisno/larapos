caeb52db223e58f986d8a4620e136f9dd4d22aa3
Ryan Sutrisno <ryan.sutrisno@gmail.com>
2020-02-17 17:27:09 +0700

    membuat manajemen produk

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,170 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Category;
+use App\Product;
+use File;
+use Image;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $products = Product::with('category')->orderBy('created_at', 'DESC')->paginate(10);
+        return view('products.index', compact('products'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $categories = Category::orderBy('name', 'ASC')->get();
+        return view('products.create', compact('categories'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'code' => 'required|string|max:10|unique:products',
+            'name' => 'required|string|max:100',
+            'description' => 'nullable|string|max:100',
+            'stock' => 'required|integer',
+            'price' => 'required|integer',
+            'category_id' => 'required|exists:categories,id',
+            'photo' => 'nullable|image|mimes:jpg,png,jpeg'
+        ]);
+
+        try {
+            $photo = null;
+            if ($request->hasFile('photo')) {
+                $photo = $this->saveFile($request->name, $request->file('photo'));
+            }
+
+            $product = Product::create([
+                'code' => $request->code,
+                'name' => $request->name,
+                'description' => $request->description,
+                'stock' => $request->stock,
+                'price' => $request->price,
+                'category_id' => $request->category_id,
+                'photo' => $request->photo
+            ]);
+            return redirect(route('produk.index'))->with(['success' => '<strong>' . $product->name . '</strong> Ditambahkan']);
+        } catch (\Exception $e) {
+            return redirect()->back()->with(['error' => $e->getMessage()]);
+        }
+    }
+
+    private function saveFile($name, $photo)
+    {
+        $images = str_slug($name) . time() . '.' . $photo->getClientOriginaExtension();
+
+        $path = public_path('uploads/product');
+
+        if (!File::isDirectory($path)) {
+            File::makeDirectory($path, 0777, true, true);
+        }
+
+        Image::make($photo)->save($path . '/' . $images);
+        return $images;
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $products = Product::findOrFail($id);
+        $categories = Category::orderBy('name', 'ASC')->get();
+        return view('products.edit', compact('products', 'categories'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $this->validate($request, [
+            'code' => 'required|string|max:10|exists:products,code',
+            'name' => 'required|string|max:100',
+            'description' => 'nullable|string|max:100',
+            'stock' => 'required|integer',
+            'price' => 'required|integer',
+            'category_id' => 'required|exists:categories,id',
+            'photo' => 'nullable|image|mimes:jpg,png,jpeg'
+        ]);
+
+        try {
+            $product = Product::findOrFail($id);
+            $photo = $product->photo;
+
+            if ($request->hasFile('photo')) {
+                !empty($photo) ? File::delete(public_path('uploads/product/' . $photo)):null;
+                $photo = $this->saveFile($request->name, $request->file('photo'));
+            }
+
+            $product->update([
+                'name' => $request->name,
+                'description' => $request->description,
+                'stock' => $request->stock,
+                'price' => $request->price,
+                'category_id' => $request->category_id,
+                'photo' => $photo
+            ]);
+            return redirect(route('product.index'))->with(['success' => '<strong>' . $product->name . '</strong> Diperbaharui']);
+        } catch (\Exception $e) {
+            return redirect()->back()->with(['error' => $e->getMessage()]);
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $products = Product::findOrFail($id);
+
+        if(!empty($products->photo)) {
+            File::delete(public_path('uploads/product/' . $products->photo));
+        }
+        $products->delete();
+        return redirect()->back()->with(['success' => '<strong>' . $products->name . '</strong> Telah dihapus!']);
+    }
+}
